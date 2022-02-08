e8989783077af0fd40978ddc93128da1328249c7
DamianFox <d.fossa.91@gmail.com>
2022-02-08 12:16:17 +0100

    Change api call with api key specified in the header

--- src/App.jsx
+++ src/App.jsx
@@ -10,19 +10,13 @@ import './App.css'
 
 function App() {
 
+  const [apiData, setApiData] = useState([])
+
   const loadData = async () => {
-    return getToken().then(token => {
-      // get the token
-      if(token.payload !== 403) {
-        const jar = {jar: token.cookieJar, withCredentials: true };
-        const headers = {headers: { 'X-CSRF-Token': token.payload }};
-        axios.get(process.env.REACT_APP_API_URL+"/rest/public-pos", jar, headers)
-        .then((res) => {
-          console.log("res.data", res.data)
-        })
-      } else {
-        console.log("Not authorised")
-      }
+    const headers = {headers: {'api-key': process.env.REACT_APP_POWER_API_KEY}}
+    axios.get(process.env.REACT_APP_API_URL+"/rest/public-pos", headers)
+    .then((res) => {
+      setApiData(res.data)
     })
   }
 
@@ -32,7 +26,7 @@ function App() {
 
   return (
     <div className="App">
-      <Offers offers={offers} />
+      <Offers offers={apiData} />
     </div>
   );
 }
