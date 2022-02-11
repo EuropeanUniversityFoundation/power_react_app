8bae5aa62a560af992d91fe6bfa300ff0e2d11b0
DamianFox <d.fossa.91@gmail.com>
2022-02-11 15:28:45 +0100

    Add react-bootstrap components

--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -1,15 +1,13 @@
 import { useState, useEffect } from 'react'
-import { Row } from 'react-bootstrap'
-import { Link, useParams, useLocation, useNavigate } from "react-router-dom"
+import { Row, Col } from 'react-bootstrap'
+import { Link, useParams, useLocation } from "react-router-dom"
 import axios from 'axios'
 
 function Offer(props) {
 
   const [error, setError] = useState(false)
   const [offer, setOffer] = useState([])
-  const [initialOffer, setInitialOffer] = useState([])
   
-  const navigate = useNavigate()
   const params = useParams()
   const location = useLocation()
   const { isPublic } = location.state
@@ -19,165 +17,171 @@ function Offer(props) {
     const headers = {headers: {'api-key': process.env.REACT_APP_POWER_API_KEY}}
     axios.get(process.env.REACT_APP_API_URL+endpoint+params.id, headers)
     .then((res) => {
-      console.log(res.data[0])
       setOffer(res.data[0])
-      setInitialOffer(res.data[0])
     })
     .catch((error) => {
       setError(true)
     })
   }
 
-  console.log("offer", offer)
-
   useEffect(() => {
     loadData()
   }, [])
+
+  if(error) {
+    return (
+      <div className="container">
+        <Row className="mt-4">
+          <p>{'Error on loading data. Try again later'}</p>
+        </Row>
+      </div>
+    )
+  }
 
   if (Object.keys(offer).length !== 0) {
 
     return (
       <div className="container">
         <Row className="mt-4">
-          <div className="col-lg-8">
+          <Col sm={12}>
+            <Link to={"/"}>
+              <i className="bi-arrow-bar-left"></i>Back to list
+            </Link>
+          </Col>
+        </Row>
+        <Row className="mt-4">
+          <Col lg={8}>
             <h5 className="text-secondary mt-3">Job details</h5>
-            <div className="row">
-              <div className="col-sm-12 col-md-12">
+            <Row>
+              <Col sm={12} md={12}>
                 <div className="card mb-3 shadow-sm">
                   <div className="card-body">
                     <h3 className="card-title text-primary">{offer.field_job_title}</h3>
-                    <div className="row mt-2">
-                      <div className="col-sm-12">
+                    <Row className="mt-2">
+                      <Col sm={12}>
                         <i className="bi-building"></i>{offer.field_company_name}
-                      </div>
-                    </div>
-                    <div className="row mt-1">
-                      <div className="col-sm-6 text-secondary">
+                      </Col>
+                    </Row>
+                    <Row className="mt-1">
+                      <Col sm={6} className="text-secondary">
                         <i className="bi-geo-alt"></i>{offer.field_job_city}, {offer.field_job_country}
-                      </div>
-                    </div>
+                      </Col>
+                    </Row>
                     <hr/>
-                    <div className="row mt-3">
-                      <div className="col-sm-12">
+                    <Row className="mt-3">
+                      <Col sm={12} >
                         <h5>Job description</h5>
-                      </div>
+                      </Col>
                       <div
                         dangerouslySetInnerHTML={{
                           __html: offer.field_job_description
                       }}/>
-                    </div>
-                    <div className="row mt-3">
-                      <div className="col-sm-12">
+                    </Row>
+                    <Row className="mt-3">
+                      <Col sm={12}>
                         <h5>Responsibilities</h5>
-                      </div>
+                      </Col>
                       <div
                         dangerouslySetInnerHTML={{
                           __html: offer.field_job_responsibilities
                       }}/>
-                    </div>
-                    <div className="row mt-3">
-                      <div className="col-sm-12">
+                    </Row>
+                    <Row className="mt-3">
+                      <Col sm={12}>
                         <h5>Required skills</h5>
-                      </div>
+                      </Col>
                      <div
                         dangerouslySetInnerHTML={{
                          __html: offer.field_applicant_required_skills
                      }}/>
-                    </div>
-                    <div className="row mt-3 mb-2">
-                      <div className="col-sm-12">
+                    </Row>
+                    <Row className="mt-3 mb-2">
+                      <Col sm={12}>
                         <a href={`mailto:${offer.field_contact_email}`} className="btn btn-primary float-right">
                           <i className="bi-send"></i>Apply via email
                         </a>
-                      </div>
-                    </div>
+                      </Col>
+                    </Row>
                   </div>
                 </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-lg-4 mb-3">
+              </Col>
+            </Row>
+          </Col>
+          <Col lg={4} className="mb-3">
             <h5 className="text-secondary pt-3">General Information</h5>
             <Row>
-              <div className="col-6">
+              <Col>
                 <i className="bi-eye" title="Public or this institution only"></i>Availability
-              </div>
-              <div className="col-6">
+              </Col>
+              <Col>
                 {offer.field_target_institution === "" ? "This institution only" : offer.field_target_institution}
-              </div>
+              </Col>
             </Row>
             <Row>
-              <div className="col-6">
+              <Col>
                 <i className="bi-alarm"></i>Apply until
-              </div>
-              <div className="col-6">
+              </Col>
+              <Col>
                 {offer.field_job_application_deadline}
-              </div>
+              </Col>
             </Row>
             <Row>
-              <div className="col-6">
+              <Col>
                 <i className="bi-calendar"></i>Duration
-              </div>
-              <div className="col-6">
+              </Col>
+              <Col>
                 {offer.field_job_duration}
-              </div>
+              </Col>
             </Row>
             <Row>
-              <div className="col-6">
+              <Col>
                 <i className="bi-speedometer"></i>Commitment
-              </div>
-              <div className="col-6">
+              </Col>
+              <Col>
                 {offer.field_job_commitment}
-              </div>
+              </Col>
             </Row>
             <Row>
-              <div className="col-6">
+              <Col>
                 <i className="bi-currency-exchange"></i>Compensation
-              </div>
-              <div className="col-6">
+              </Col>
+              <Col>
                 {offer.field_job_compensation}
-              </div>
+              </Col>
             </Row>
             <h5 className="text-secondary mt-3">Contact information</h5>
             <Row>
-              <div className="col-6">
+              <Col>
                 <i className="bi-person"></i>Name
-              </div>
-              <div className="col-6">
+              </Col>
+              <Col>
                 {offer.field_contact_first_name}{" "}{offer.field_contact_last_name}
-              </div>
+              </Col>
             </Row>
             <Row>
-              <div className="col-6">
+              <Col>
                 <i className="bi-envelope"></i>Email
-              </div>
-              <div className="col-6">
+              </Col>
+              <Col>
                 <a href={`mailto:${offer.field_contact_email}`}>{offer.field_contact_email}</a>
-              </div>
+              </Col>
             </Row>
             <Row>
-              <div className="col-6">
+              <Col>
                 <i className="bi-telephone"></i>Phone
-              </div>
-              <div className="col-6">
+              </Col>
+              <Col>
                 {offer.field_contact_phone_no}
-              </div>
+              </Col>
             </Row>
-          </div>
+          </Col>
         </Row>
-        <div className="row mb-2">
-          <div className="col-sm-12">
-            <Link to={"/"}>
-              <i className="bi-arrow-bar-left"></i>Back to list
-            </Link>
-          </div>
-        </div>
       </div>
     )
   } else {
     return (
       <div className="container">
-        <Row className="mt-4">
+        <Row className="mt-5">
           <p>{'Loading data...'}</p>
         </Row>
       </div>
