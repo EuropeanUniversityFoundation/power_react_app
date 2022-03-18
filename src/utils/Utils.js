5295ed3c06d9d5dcef96ae7917945b4bd3931373
DamianFox <d.fossa.91@gmail.com>
2022-03-18 15:48:18 +0100

    Add function to convert country codes into country names

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -1,7 +1,14 @@
 
+const iso3166 = require('./../data/iso3166_cc2name.json')
+
 export function parseValue(value) {
   if(value == null || value === ""){
     return "Not provided"
   }
   return value
+}
+
+export function codeToName(code) {
+  let countryName = iso3166[code] ? iso3166[code] : ""
+  return countryName
 }
